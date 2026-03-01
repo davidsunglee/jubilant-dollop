8a8e8d85c6b0de19278f615f61b5531b34276a5f
David Lee <davidsunglee@gmail.com>
2026-02-28 20:11:58 -0500

    chore: clean up unused environment properties, update Xcode project for new files
    - Remove obstacleColor and groundColor from GameEnvironment
    - Remove PipeNode.swift from Xcode project
    - Add ObstacleNode, EnvironmentRenderer, and all 6 environment renderers to Xcode project

--- FlappyBird/Models/GameEnvironment.swift
+++ FlappyBird/Models/GameEnvironment.swift
@@ -32,28 +32,6 @@ enum GameEnvironment: String, CaseIterable, Identifiable {
         }
     }
 
-    var obstacleColor: SKColor {
-        switch self {
-        case .classic:    return .green
-        case .jungle:     return .brown
-        case .underwater: return .systemPink
-        case .arctic:     return .systemBlue
-        case .desert:     return .orange
-        case .space:      return .purple
-        }
-    }
-
-    var groundColor: SKColor {
-        switch self {
-        case .classic:    return SKColor(red: 0.86, green: 0.69, blue: 0.35, alpha: 1.0)
-        case .jungle:     return SKColor(red: 0.2, green: 0.4, blue: 0.1, alpha: 1.0)
-        case .underwater: return SKColor(red: 0.1, green: 0.15, blue: 0.4, alpha: 1.0)
-        case .arctic:     return SKColor(red: 0.9, green: 0.95, blue: 1.0, alpha: 1.0)
-        case .desert:     return SKColor(red: 0.76, green: 0.6, blue: 0.3, alpha: 1.0)
-        case .space:      return SKColor(red: 0.1, green: 0.0, blue: 0.2, alpha: 1.0)
-        }
-    }
-
     var renderer: EnvironmentRenderer {
         switch self {
         case .classic:    return ClassicEnvironmentRenderer()
